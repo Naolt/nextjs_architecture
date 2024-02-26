bcf704e8c451293ecd0d742aadb5799a87d5b6f9
naolt <naoltamrat11@gmai.com>
2024-02-26 17:30:47 +0300

    format fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import Image from 'next/image';
 
-const llo: Number = 'bello';
-const glo: Number = 'bello';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
